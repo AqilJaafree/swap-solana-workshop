3ffa130cd0c3cc2e63628ebf954c03c1d33f252c
Aqil Jaafree <wanaqilre@gmail.com>
2025-02-10 23:44:57 +0800

    Feat: adjusted lib

---
+++ programs/swap/src/instructions/make_offer.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+#[derive(Accounts)]
+
+pub struct MakeOffer {
+
+}
+
+pub fn handler(ctx: Context<>) -> Result <()> {
+    
+} 
--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -1,16 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
 use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
 
 declare_id!("7aa35rvHUZ3BZfnkg9j51f8CMEMf9hC9etxiZiAdwD6E");
 
 #[program]
 pub mod swap {
+    use crate::instruction::MakeOffer;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn make_offer(ctx: Context<MakeOffer>) -> Result<()> 
+    {
+        instructions::make_offer::send_offered_tokens_to_vault ()?;
+        instructions::make_offer::save_offer()    
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
